ff01c5a9c3848bb6bc1653b220a40f1aedc733b8
TzvikaShtainer <TzvikaShtainer@gmail.com>
2024-09-08 19:01:11 +0300

    Adding FishSword Animation

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -12,27 +12,22 @@ public class BackgroundScroller : MonoBehaviour
 
     private void Start()
     {
-        // Assuming all backgrounds have the same width
         backgroundWidth = backgroundsArray[0].GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     private void Update()
     {
-        // Move each background to the left
         foreach (GameObject background in backgroundsArray)
         {
             background.transform.position += Vector3.left * (speed * Time.deltaTime);
         }
-
-        // Check if any background has moved completely off-screen
+        
         foreach (GameObject background in backgroundsArray)
         {
             if (background.transform.position.x <= -backgroundWidth)
             {
-                // Find the rightmost background
                 GameObject rightmostBackground = GetRightmostBackground();
                 
-                // Reposition the off-screen background to the right of the rightmost one
                 Vector3 newPosition = new Vector3(rightmostBackground.transform.position.x + backgroundWidth, 
                     background.transform.position.y, 
                     background.transform.position.z);
@@ -40,11 +35,11 @@ public class BackgroundScroller : MonoBehaviour
             }
         }
     }
-
-    // Helper function to find the rightmost background
+    
     private GameObject GetRightmostBackground()
     {
         GameObject rightmost = backgroundsArray[0];
+        
         foreach (GameObject background in backgroundsArray)
         {
             if (background.transform.position.x > rightmost.transform.position.x)
