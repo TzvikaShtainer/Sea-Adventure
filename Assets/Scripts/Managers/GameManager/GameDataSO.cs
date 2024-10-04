abc8b29aef6a23208976f0a9f3e629fe8f37e71f
TzvikaShtainer <TzvikaShtainer@gmail.com>
2024-10-04 14:28:45 +0300

    Remove SO Data

--- Assets/Scripts/Managers/GameManager/GameDataSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GameData", menuName = "ScriptableObjects/GameData", order = 1)]
-public class GameDataSO : ScriptableObject
-{
-    public float MaxDistanceTraveled; 
-    public int MoneyAmount; 
-}
