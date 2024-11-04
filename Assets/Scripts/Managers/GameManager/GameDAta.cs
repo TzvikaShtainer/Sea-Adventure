8d11533620c332561648673f139082a78ad4e7da
TzvikaShtainer <TzvikaShtainer@gmail.com>
2024-11-04 13:29:59 +0200

    Fixing Init Of Inventory

--- Assets/Scripts/Managers/GameManager/GameDAta.cs
+++ Assets/Scripts/Managers/GameManager/GameDAta.cs
@@ -19,22 +19,21 @@ namespace DefaultNamespace.GameManager
 
         public GameData()
         {
-            // Initialize default values
             MaxDistanceTraveled = 0f;
             MoneyAmount = 0;
             tutorialCompleted = false;
             
             inventoryItemIDs = new List<string>();
 
-            // if (!inventoryItemIDs.Contains("ShopItem_BG_Blue_Default"))
-            // {
-            //     inventoryItemIDs.Add("ShopItem_BG_Blue_Default");
-            // }
-            //
-            // if (!inventoryItemIDs.Contains("ShopItem_default_Character"))
-            // {
-            //     inventoryItemIDs.Add("ShopItem_default_Character");
-            // }
+             if (!inventoryItemIDs.Contains("ShopItem_BG_Blue_Default"))
+             {
+                 inventoryItemIDs.Add("ShopItem_BG_Blue_Default");
+             }
+            
+             if (!inventoryItemIDs.Contains("ShopItem_default_Character"))
+             {
+                 inventoryItemIDs.Add("ShopItem_default_Character");
+             }
 
             bgCurrentColor = "ShopItem_BG_Blue_Default";
             
