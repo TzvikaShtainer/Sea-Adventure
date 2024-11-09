925d8eb516bae8e3a786d0d8a22671d127199d69
TzvikaShtainer <TzvikaShtainer@gmail.com>
2024-11-09 13:10:21 +0200

    Fixing Colliders In Players Skins

--- Assets/Scripts/Shop&InventorySystems/ShopitemEffects/ChangeMainCharacterEffect.cs
+++ Assets/Scripts/Shop&InventorySystems/ShopitemEffects/ChangeMainCharacterEffect.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 public class ChangeMainCharacterEffect : ScriptableObject, IShopItemEffect
 {
      public RuntimeAnimatorController  newAnimatorController;
+
+     [Header("CapsuleCollider2D 1")] 
+     public Vector2[] CollidersOffsets;
+     
+     // [Header("CapsuleCollider2D 2")]
+     // public CapsuleCollider2D collider2;
+     // public int offsetX2;
+     // public int offsetY2;
+     //
+     // [Header("CapsuleCollider2D 1")]
+     // public CapsuleCollider2D collider3;
+     // public int offsetX3;
+     // public int offsetY3;
+     //
+     // [Header("CircleCollider2D 4")]
+     // public CircleCollider2D collider4;
+     // public int offsetX4;
+     // public int offsetY4;
     
     public void ApplyEffect(ShopItem shopItem)
     {
-        EffectsHandler.Instance.ChangeMainCharacter(newAnimatorController);
+        EffectsHandler.Instance.ChangeMainCharacter(newAnimatorController, CollidersOffsets);
     }
 }
--- Assets/Scripts/Shop&InventorySystems/ShopitemEffects/EffectsHandler.cs
+++ Assets/Scripts/Shop&InventorySystems/ShopitemEffects/EffectsHandler.cs
@@ -6,6 +6,8 @@ public class EffectsHandler : MonoBehaviour
     public static EffectsHandler Instance { get; private set; }
     
     [SerializeField] private GameObject player;
+    [SerializeField] private Collider2D[] colliders;
+
 
     private void Awake()
     {
@@ -25,9 +27,32 @@ public class EffectsHandler : MonoBehaviour
         BackGroundManager.Instance.ChangeBackGround(backgroundSprites);
     }
 
-    public void ChangeMainCharacter(RuntimeAnimatorController newAnimatorController)
+    public void ChangeMainCharacter(RuntimeAnimatorController newAnimatorController, Vector2[] offset)
     {
         PlayerAnimationController playerAnimationController = player.GetComponent<PlayerAnimationController>();
         playerAnimationController.SetAnimator(newAnimatorController);
+        
+        colliders = player.GetComponents<Collider2D>();
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            SetOffsetX(offset[i].x, i);
+
+            SettOffsetY(offset[i].y, i);
+        }
+    }
+    
+    private void SetOffsetX(float offsetX, int i)
+    {
+        Vector2  centerX = colliders[i].offset;
+        centerX.x = offsetX;
+        colliders[i].offset = centerX;
+    }
+    
+    private void SettOffsetY(float offsetY, int i)
+    {
+        Vector2  centerY = colliders[i].offset;
+        centerY.y = offsetY;
+        colliders[i].offset = centerY;
     }
 }
