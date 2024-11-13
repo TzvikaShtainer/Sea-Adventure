c971e689f300b120c033cf5d12cb3294a9442137
TzvikaShtainer <TzvikaShtainer@gmail.com>
2024-11-13 11:12:25 +0200

    Adding Death Effect(Not Working Yet)

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -9,14 +9,35 @@ namespace Player
         [SerializeField] private PlayerMovement playerMovement;
 
         private const string IS_Jumping = "IsJumping";
+        private const string IS_Dead = "IsDead";
+
+        private void OnEnable()
+        {
+            PlayerHealthSystem.onDeath += PlayerHealthSystem_OnDeath;
+        }
+        private void OnDisable()
+        {
+            PlayerHealthSystem.onDeath -= PlayerHealthSystem_OnDeath;
+        }
+        
+
         private void Update()
         {
             animator.SetBool(IS_Jumping, playerMovement.GetJumpState());
+            Debug.Log(animator.GetBool(IS_Jumping));
+            Debug.Log(animator.GetBool(IS_Dead));
         }
 
         public void SetAnimator(RuntimeAnimatorController  newAnimatorController)
         {
             animator.runtimeAnimatorController = newAnimatorController;
+        }
+        
+        private void PlayerHealthSystem_OnDeath()
+        {
+            Debug.Log("Player is dead");
+            //animator.updateMode = AnimatorUpdateMode.UnscaledTime; //changed in the component itself
+            animator.SetBool(IS_Dead, true);
         }
     }
 }
